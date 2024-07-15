4f3594a63a477dbd272cdc6fbbd1c53bb6f9e1b1
Chaos-19 <kalgetachew375@gmail.com>
2024-07-15 16:34:59 +0300

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core"
+import { RouterOutlet } from "@angular/router"
+import { NavBarComponent } from "./shared/nav-bar/nav-bar.component"
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: "app-root",
+    standalone: true,
+    imports: [RouterOutlet, NavBarComponent],
+    templateUrl: "./app.component.html",
+    styleUrl: "./app.component.css"
 })
 export class AppComponent {
-  title = 'my-blog-app';
+    title = "my-blog-app"
 }
