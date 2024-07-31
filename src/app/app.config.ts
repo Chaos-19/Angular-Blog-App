375412aad87e109a1edaacf3eaf39db80069a13a
kalkidan getachew <kalgetachew375@gmail.com>
2024-07-31 11:44:42 +0300

    add config for http module

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core"
 import { provideRouter, withHashLocation } from "@angular/router"
 import { provideMarkdown } from "ngx-markdown"
+import { provideHttpClient } from "@angular/common/http";
 
 import { routes } from "./app.routes"
 
@@ -8,6 +9,7 @@ export const appConfig: ApplicationConfig = {
     providers: [
         provideZoneChangeDetection({ eventCoalescing: true }),
         provideRouter(routes, withHashLocation()),
-        provideMarkdown()
+        provideMarkdown(),
+        provideHttpClient()
     ]
 }
